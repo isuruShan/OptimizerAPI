6db89f3a4f9c7a12ab3ee505f64ff8e364b4980a
igayashan <isurugy@gmail.com>
2021-09-08 22:35:37 +0530

    Initial commit

---
+++ src/main/java/com/treset/optimizer_api/OptimizerApiApplication.java
@@ -0,0 +1,14 @@
+package com.treset.optimizer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimizerApiApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/treset/optimizer_api/OptimizerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treset.optimizer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
